7641f0d6f3500ba57d2c1c6b4b42d9d38869696c
toh14 <toh14@DESKTOP-5V5H96E>
2024-05-19 23:47:49 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService 
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) 
+            => _context = context;
+
+        public async Task<Order?> GetOrder() //return the newest order ich have more than one item
+        {
+            if (_context != null)
+                return await _context.Orders.Where(o => o.Quantity > 1)
+                                            .OrderByDescending(_ => _.CreatedAt)
+                                            .FirstOrDefaultAsync();
+            else throw new Exception("Database has no data");
+        }
+
+        public async Task<List<Order>> GetOrders() //return orders from active users and sorted by creation date
+        {
+            if (_context != null)
+                return await _context.Orders.Select(o => new
+                {
+                    Order = o,
+                    Status = o.User.Status
+                })
+                .Where(_ => _.Status == UserStatus.Active)
+                .Select(_ => _.Order)
+                .OrderBy(_ => _.CreatedAt)
+                .ToListAsync();
+            else throw new Exception("Database has no data");
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService 
+    {
+        private readonly ApplicationDbContext? _context;
+
+        public UserService(ApplicationDbContext? context) => _context = context;
+
+        public async Task<User?> GetUser()         //return a User with max total sum of delivered orders at 2003
+        {
+
+
+            DateTime date = new(2003, 1, 1, 0, 0, 0);
+
+            if (_context != null)
+                return await _context.Users.Select(u => new
+                {
+                    User = u,
+                    Total = u.Orders.Where(o => o.Status == OrderStatus.Delivered && o.CreatedAt >= date && o.CreatedAt < date.AddYears(1)).Sum(o => o.Price)
+                })
+                .OrderByDescending(_ => _.Total)  //Sorts in descending
+                .Select(_ => _.User)
+                .FirstOrDefaultAsync();
+            else throw new Exception("Database has no data"); 
+        }
+
+
+        async Task<List<User>> IUserService.GetUsers()  //return Users with paid orders from 2010
+        {
+            DateTime date = new(2010, 1, 1, 0, 0, 0);
+
+            if (_context != null)
+                return await _context.Users.Select(u => new
+                {
+                    User = u,
+                    PaidOrders = u.Orders.Where(o => o.Status == OrderStatus.Paid && o.CreatedAt >= date && o.CreatedAt < date.AddYears(1)).ToList()
+                })
+                .Where(_ => _.PaidOrders.Count > 0)  //checking paidOrderList for is it not null
+                .Select(_ => _.User)
+                .ToListAsync();
+            else throw new Exception("Database has no data");
+        } 
+    }
+}
